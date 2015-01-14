f0fd3a53b90ee5798f176bfd5a60aa70ca9b57dc
fischerbastian <bastian.fischerg@gmail.com>
2015-01-14 11:55:01 -0300

    se crea script para crear sede en reserva de salas y se reestructura carpeta de emarking

---
+++ tests/acceptance/03reserva_salas/003Create_Campus.php
@@ -0,0 +1,20 @@
+<?php
+
+// Log In Admin
+$I = new AcceptanceTester($scenario);
+$I->wantTo('Create 1 Campus');
+$I->amOnPage('/?lang=en');
+$I->click('Log in', 'div.logininfo');
+$I->fillField('username', 'admin');
+$I->fillField('password', 'pepito.P0');
+$I->click('loginbtn');
+$I->see('You are logged in as', 'div.logininfo');
+$I->seeLink('Admin Usuario');
+
+$I->amOnPage('local/reservasalas/sedes.php?lang=en');
+$I->see('Create new campus');
+$I->click('Create new campus');
+$I->fillField('sede', 'Peñalolen');
+$I->click('submitbutton');
+$I->see('Peñalolen');
+?>
