1ea4c2ece2482bf9697ae5c00abbc5fcf9625dfb
Gabriel Contreras <gacontreras@alumnos.uai.cl>
2015-01-19 15:51:34 -0300

    Modificacion en script 006 de asistencias

--- tests/acceptance/04asistencias/006Login_Student_And_Check_AttendanceCept.php
+++ tests/acceptance/04asistencias/006Login_Student_And_Check_AttendanceCept.php
@@ -2,24 +2,40 @@
 // Loguear como Alumnmo
 $I = new AcceptanceTester($scenario);
 $I->wantTo('Log in as a student and check attendance');
-$I->amOnPage('/?lang=es');
-$I->click('Entrar', 'div.logininfo');
+
+//Entrar como profesor y empezar a pasar lista
+$I->amOnPage('/?lang=en');
+$I->click('Log in', 'div.logininfo');
+$I->fillField('username', 'profesor1');
+$I->fillField('password', 'pepito.P0');
+$I->click('loginbtn');
+$I->see('You are logged in as ');
+$I->seeLink('PROFESOR 1');
+$I->amOnPage('/?lang=en');
+$I->click('Test Course A');
+$course_id = $I->grabFromCurrentUrl('/id=(\d+)/');
+$I->amOnPage('/course/view.php?id='.$course_id); // esto comprueba que tomo la id satisfactoriamente
+$I->amOnPage('/local/attendance/attendance.php?courseid='.$course_id);
+$I->click('//section[@id="region-main"]/div[2]/div/form/div/input');
+$I->see('Close this session');
+$I->click('Log out');
+
+// Loguear como Alumnmo
+$I->amOnPage('/login/index.php?lang=en');
+
+$I->seeElement('input#username');
 $I->fillField('username', 'alumno1');
 $I->fillField('password', 'pepito.P0');
 $I->click('loginbtn');
-$I->see('Usted se ha identificado como', 'div.logininfo');
-$I->seeLink('Alumno 1');
-
-
-// Checkear Block UAI
-$I->see('UAI','div.block_uai.block.block_navigation');
-$I->click('Asistencias');
-$I->see('Marcar asistencia');
+$I->see('You are logged in as ');
+$I->seeLink('ALUMNO 1');
 
 // Marcar asistencia
-$I->click('Marcar asistencia');
-$I->amOnPage('');	// insertar direccion de pagina a la cual se redirige
-$I->see('Se registró tu asistencia a');
-$I->click('Volver');
-$I->amOnPage('');	// insertar direccion de pagina a la cual se redirige
+$I->amOnPage('/local/attendance/attendance.php?courseid='.$course_id);
+$I->seeElement('//a[@href="http://localhost/moodle/local/attendance/markattendance.php?courseid=1"]');
+$I->amOnPage('/local/attendance/markattendance.php?courseid=1');	
+$I->see('Mark Attendance');
+$I->click('//input[@value="Mark Attendance"]');
+$I->see('Attendance marked');
+
 ?>
