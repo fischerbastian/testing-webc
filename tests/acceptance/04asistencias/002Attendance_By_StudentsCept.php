61b0370f54bf9aa6edcbd05b45a9d667cd690f78
Gabriel Contreras <gacontreras@alumnos.uai.cl>
2015-01-20 11:52:21 -0300

    Finalizacion de Script 002 de asistencias (manera rudimentaria)

--- tests/acceptance/04asistencias/002Attendance_By_StudentsCept.php
+++ tests/acceptance/04asistencias/002Attendance_By_StudentsCept.php
@@ -10,16 +10,80 @@ $U = new UserController($I);
 $I->wantTo('Check attendance by students');
 $U->login('profesor1', 'pepito.P0','PROFESOR 1');
 
+
 //Entrar al curso
 $I->amOnPage('/?lang=en');
 $I->click('Test Course A');
 $course_id = $I->grabFromCurrentUrl('/id=(\d+)/');
-$I->amOnPage('/user/index.php?id='.$course_id);
-$table = $I->grabTextFrom(".//*[@id='participants']/table");
+$I->amOnPage('/course/view.php?id='.$course_id);
 
 // Ver asistencia por alumno
 $I->amOnPage('/local/attendance/viewstudentrecord.php?courseid='.$course_id);
 $I->see('Attendance Record');
+
+// Alumno 1
+$I-> seeElement('//section[@id="region-main"]/div[2]/table/tbody/tr/td[4]/a/img');
+$I->amOnPage("/local/attendance/viewstudentrecord.php?action=view_student_details&courseid=3&userid=3");
+$I->see('Total');
+// $token = $I->grabTextFrom('#yui_3_17_2_2_1421700648904_11');  ++ no existe forma de hacer grab al texto
+$I->checkoption('//td[3]/input');		 
+$I->selectOption('#id_action_for_checked','Mark as absent');
+$I->click('Apply');
+// $I->dontsee($token);
+
+// Alumno 2
+$I->amOnPage('/local/attendance/viewstudentrecord.php?courseid='.$course_id);
+$I-> seeElement('//tr[2]/td[4]/a/img');
+$I->amOnPage("/local/attendance/viewstudentrecord.php?action=view_student_details&courseid=3&userid=4");
+$I->see('Total');
+// $token = $I->grabTextFrom('#yui_3_17_2_2_1421700648904_11');  ++ no existe forma de hacer grab al texto
+$I->checkoption('//td[3]/input');		
+$I->selectOption('#id_action_for_checked','Mark as absent');
+$I->click('Apply');
+// $I->dontsee($token);
+
+// Alumno 3
+$I->amOnPage('/local/attendance/viewstudentrecord.php?courseid='.$course_id);
+$I-> seeElement('//a[@href="http://localhost/moodle/local/attendance/viewstudentrecord.php?action=view_student_details&courseid=3&userid=5"]');
+$I->amOnPage("/local/attendance/viewstudentrecord.php?action=view_student_details&courseid=3&userid=5");
+$I->see('Total');
+// $token = $I->grabTextFrom('#yui_3_17_2_2_1421700648904_11');  ++ no existe forma de hacer grab al texto
+$I->checkoption('//td[3]/input');		
+
+$I->selectOption('#id_action_for_checked','Mark as absent');
+$I->click('Apply');
+// $I->dontsee($token);
+
+// Alumno 4
+$I->amOnPage('/local/attendance/viewstudentrecord.php?courseid='.$course_id);
+$I-> seeElement('//section[@id="region-main"]/div[2]/table/tbody/tr[4]/td[4]/a/img');
+$I->amOnPage("/local/attendance/viewstudentrecord.php?action=view_student_details&courseid=3&userid=6");
+$I->see('Total');
+// $token = $I->grabTextFrom('#yui_3_17_2_2_1421700648904_11');  ++ no existe forma de hacer grab al texto
+$I->checkoption('//td[3]/input');		
+$I->selectOption('#id_action_for_checked','Mark as absent');
+$I->click('Apply');
+// $I->dontsee($token);
+
+// Alumno 5
+$I->amOnPage('/local/attendance/viewstudentrecord.php?courseid='.$course_id);
+$I-> seeElement('//a[@href="http://localhost/moodle/local/attendance/viewstudentrecord.php?action=view_student_details&courseid=3&userid=7"]');
+$I->amOnPage("/local/attendance/viewstudentrecord.php?action=view_student_details&courseid=3&userid=7");
+$I->see('Total');
+// $token = $I->grabTextFrom('#yui_3_17_2_2_1421700648904_11');  ++ no existe forma de hacer grab al texto
+$I->checkoption('//td[3]/input');
+$I->selectOption('#id_action_for_checked','Mark as absent');
+$I->click('Apply');
+// $I->dontsee($token);
+
+
+
+
+
+
+
+
+
 
 /**
  * 
@@ -35,20 +99,6 @@ $I->see('Attendance Record');
  */
 
 
-// change attendance state
-
-$I->amOnPage('');	//rellenar con direccion de pagina
-$token = $I->grabTextFrom(''); //rellenar con informacion del div en donde aparece porcentaje
-$I->see('Asistencias de');
-$I->checkoption('');	//insertar direccion de los checkbox
-$I->checkoption('');	//insertar direccion de los checkbox
-$I->checkselectOption('Elija una acci�n','Marcar presente');
-$I->click('Aplicar');
-$I->see('Confirmaci�n');
-$I->click('Si');
-$I->dontsee($token);
-
-//Falta por terminar
 ?>
 
 	
