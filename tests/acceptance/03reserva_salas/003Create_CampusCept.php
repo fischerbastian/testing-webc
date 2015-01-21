25759950c1ad167a3603168fa60e028dc9d189dd
fischerbastian <bastian.fischerg@gmail.com>
2015-01-21 20:38:17 -0300

    cambio en script de creacion de sedes

--- tests/acceptance/03reserva_salas/003Create_CampusCept.php
+++ tests/acceptance/03reserva_salas/003Create_CampusCept.php
@@ -1,77 +1,36 @@
 <?php
 
 use Codeception\Module\UserController;
+use Codeception\Module\ReservaController;
 
-$uai = array('Peñalolén' => array('Edificio A' => array('Class' => array('101A','102A'),
-														  			  'Reunion' => array('sala1','sala2'),
-														  			  'Study' => array('01','02','03')
-														  ), 
-												'Edificio B' => array('Class' => array('101B','102B'),
-														  			  'Reunion' => array('sala3','sala4'),
-														  			  'Study' => array()
-														  ), 
-												'Edificio C' => array('Class' => array('101C','102C'),
-														  			  'Reunion' => array('sala5','sala6','sala7'),
-														  			  'Study' => array('01','02')
-														  ),
-												'Edificio D' => array('Class' => array('101D','102D'),
-																	  'Reunion' => array('sala8','sala9'),
-																	  'Study' => array()
-														  ),
-		    									'Edificio E' => array('Class' => array('101E','102E'),
-														  			  'Reunion' => array('sala10','sala11'),
-														  		      'Study' => array()
-														  ), 
-									) , 
-		
-			'Viña del Mar' => array('Edificio A' => array('Class' => array('101A','102A'),
-														  		'Reunion' => array('sala1','sala2'),
-														  		'Study' => array('01','02')
-														  ), 
-										   'Edificio B' => array('Class' => array('101B','102B'),
-														  		'Reunion' => array('sala1','sala2'),
-														  		'Study' => array('01','02')
-														  ), 
-										   'Edificio C' => array('Class' => array('101C','102C'),
-														  		'Reunion' => array('sala1','sala2'),
-														  		'Study' => array('01','02')
-														  ),
-										   'Edificio D' => array('Class' => array('101D','102D'),
-																 'Reunion' => array('sala1','sala2'),
-																 'Study' => array('01','02')
-														  )
-									) ,
-		
-        	'Miami' => array('Edificio A' => array('Class' => array('101A','102A'),
-														  'Reunion' => array('sala1','sala2'),
-														  'Study' => array('01','02')
-												  ) 
-							 
-							)
-									
-			);
 
 // Log In Admin
 
 $I = new AcceptanceTester($scenario);
 $U = new UserController($I);
+$R = new ReservaController($I);
 
 $I->wantTo('Create UAI Campus');
-$U->login('admin', 'pepito.P0','Admin Usuario');
+$U->login('admin', 'pepito.P0', 'Admin Usuario');
 
 $I->amOnPage('local/reservasalas/sedes.php?lang=en');
 
 $I->see('Create new campus');
 
+
+
+
 //TODO cambiar $uai array por una funcion y crear objeto
-foreach ($uai as $campus => $place){
 	
-	$I->dontSee($campus);
+for ($i = 0; $i < count($R->crear()); $i++) {
+	
+	$I->dontSee($R->crear()[$i]->nombre_sede);
 	
 	$I->click('Create new campus');
-	$I->fillField('sede', $campus);
+	$I->fillField('sede', $R->crear()[$i]->nombre_sede);
 	$I->click('submitbutton');
-	$I->see($campus);
+	$I->see($R->crear()[$i]->nombre_sede);
 }
+
 
 ?>
