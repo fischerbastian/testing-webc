9e76b06eefcc4beeb66cf1433a42805884f270f6
Bastián Fischer <bastian.fischerg@gmail.com>
2015-06-01 20:49:35 -0300

    mejora en función que verifica log in

--- tests/_support/AcceptanceHelper.php
+++ tests/_support/AcceptanceHelper.php
@@ -6,13 +6,11 @@ namespace Codeception\Module;
 class IndexPage
 {
     // URL of a page
-    public static $URL = '/?lang=en';
+    public static $URL = '/login/index.php';
     // This properties define a UI map for Login Page
-    public static $loginField = "div.logininfo";
     public static $usernameField = "username";
     public static $passwordField = "password";
     public static $submitButton = "loginbtn";
-    public static $loggedField = "div.logininfo";
 }
 
 
@@ -26,11 +24,9 @@ class UserController
 
 	public function login($username,$password,$name) {
 		$this->user->amOnPage(IndexPage::$URL);
-		$this->user->click('Log in',IndexPage::$loginField);
 		$this->user->fillField(IndexPage::$usernameField, $username);
 		$this->user->fillField(IndexPage::$passwordField, $password);
 		$this->user->click(IndexPage::$submitButton);
-		$this->user->see('You are logged in as', IndexPage::$loggedField);
 		$this->user->seeLink($name);
 	}
 }
