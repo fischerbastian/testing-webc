424e16c825ddbcafb215e621aefe20f6d5fdfc8f
Bastián Fischer <bastian.fischerg@gmail.com>
2015-06-03 20:16:29 -0300

    funcion de creación de cursos agregada a script

--- tests/acceptance/01moodle/004Create_CourseCept.php
+++ tests/acceptance/01moodle/004Create_CourseCept.php
@@ -1,25 +1,14 @@
 <?php 
 
 use Codeception\Module\LoginController;
+use Codeception\Module\CourseController;
 
 $I = new AcceptanceTester($scenario);
 $U = new LoginController($I);
+$O = new CourseController($I);
 
 $I->wantTo('Create a 2 test courses');
 $U->login('admin', 'pepito.P0', 'Admin User');
 
-// Add course
-$I->click('Add a new course');
-$I->amOnPage('course/edit.php?category=1&lang=en');
-$I->fillField('fullname', 'Test Course A');
-$I->fillField('shortname', 'test1');
-$I->selectOption('Course category ','Pregrado / Pregrado Santiago / .2014 / SEGUNDO SEMESTRE .2014 / EDIFICIO .A');
-$I->click('submitbutton');
-
-$I->amOnPage('/?lang=en');
-$I->amOnPage('course/edit.php?category=1&lang=en');
-$I->fillField('fullname', 'Test Course D');
-$I->fillField('shortname', 'test2');
-$I->selectOption('Course category ','Pregrado / Pregrado Santiago / .2014 / SEGUNDO SEMESTRE .2014 / EDIFICIO .D');
-$I->click('submitbutton');
+$O->createCourse('Test Course B', 'test2', 'Pregrado / Pregrado Santiago');
 ?>
