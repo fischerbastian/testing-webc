f4a1b183287c8a24ed67638ced03bdd334094962
Bastián Fischer <bastian.fischerg@gmail.com>
2015-07-03 16:39:42 -0300

    cambios estructurales

--- tests/acceptance/02emarking/003MarioTestCept.php
+++ tests/acceptance/02emarking/003MarioTestCept.php
@@ -8,21 +8,14 @@ $I->wantTo('');
 $U->login('admin', 'pepito.P0','Admin User');
 
 $I->click('Turn editing on');
-$I->amOnPage('course/modedit.php?add=emarking&type=&course=2&section=0&return=0&sr=0');
-
-
-/////////
-
+$I->amOnPage('course/modedit.php?add=emarking&type=&course=7&section=0&return=0&sr=0');
 
 $I->fillField('name','tu mama');
-//$I->fillField('','');
 
 $I->selectOption('totalpages','2');
 $I->selectOption('anonymous','0');
-//$I->fiilField('','');
 
 $I->checkOption('enableduedate');
-
 
 	$I->selectOption('markingduedate[day]','01');
 	$I->selectOption('markingduedate[month]','February');
@@ -30,10 +23,8 @@ $I->checkOption('enableduedate');
 	$I->selectOption('markingduedate[hour]','01');
 	$I->selectOption('markingduedate[minute]','05');
 
-
 $I->checkOption('regraderestrictdates');
 
-	
 		$I->selectOption('regradesopendate[day]','01');
 		$I->selectOption('regradesopendate[month]','February');
 		$I->selectOption('regradesopendate[year]','2015');
@@ -46,10 +37,9 @@ $I->checkOption('regraderestrictdates');
 		$I->selectOption('regradesclosedate[hour]','01');
 		$I->selectOption('regradesclosedate[minute]','10');
 	
-
 $I->selectOption('peervisibility','1');
 
-$I->selectOption('gradecat','1');
+$I->selectOption('gradecat','7');
 $I->selectOption('grademin','0');
 $I->selectOption('grade','7');
 
@@ -58,14 +48,11 @@ $I->checkOption('adjustslope');
 	$I->fillField('adjustslopegrade','1');
 	$I->fillField('adjustslopescore','1');
 	
-
 $I->fillField('cmidnumber','');
 $I->selectOption('groupmode','0');
 
-
 $I->click('id_submitbutton2');
 
-////////
-
 $I->seeLink('tu mama');
+
 ?>
